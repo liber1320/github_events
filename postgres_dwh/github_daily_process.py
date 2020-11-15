d5aa5ec2b63d845df2b1a8d4a01ce17caf6f3b9e
liber <liber1320@vp.pl>
2020-11-15 14:18:11 +0100

    update spark script

--- postgres_dwh/github_daily_process.py
+++ postgres_dwh/github_daily_process.py
@@ -3,57 +3,13 @@ import pyspark.sql.functions as f
 from pyspark import SparkContext, SparkConf
 from pyspark.sql import SparkSession
 import datetime
-import logging
 import os
-import sys 
-
-def create_spark_session():
-	conf = pyspark.SparkConf().setAppName('appName').setMaster('local')
-	sc = pyspark.SparkContext(conf = conf)
-	spark = SparkSession(sc)
-	logging.info('******* Spark session created ******* ')
-	return spark
-
-def filter_data(df):
-	"""Function filters data frame by choosing subset of events."""
-
-	df = df.filter(((df.type=="PullRequestEvent") & (df.payload.action=='opened')) | \
-		((df.type=="IssuesEvent") & (df.payload.action=='opened')) | \
-		(df.type=="ForkEvent"))
-	return df
-
-def select_columns(df):
-	"""Function select set of columns for further transformations and adds aliases"""
-
-	df = df.selectExpr(["created_at", "actor['id'] as actor_id", "actor['login'] as actor_login", \
-						"actor['display_login'] as display_login", "repo['id'] as repo_id", \
-						"repo['name'] as repo_name", "type"]) 
-	return df
-
-def process_json(year, month, day, spark):
-	"""Function processes json file for each day by: 
-	- filtering data, 
-	- calculating field,
-	- selecting columns 
-	- writing results to parquet file"""
-
-	input_file = "{}-{}-{}".format(year, str(month).zfill(2), str(day).zfill(2))
-	output_file = "df_{}.parquet".format(input_file)
-	try:
-		df = spark.read.json(input_file)
-	except Exception as exc:
-		logging.info('No data for path %s Exception: %s' % (input_file, exc))
-
-	df = filter_data(df)
-	df = select_columns(df)
-	df.write.parquet(output_file)
-
-	logging.info('******* Github data processed successfully. *******')
+import sys
+import logging
 
 def main():
-	""" Run pipeline for given month and year in format 'YYYY', 'MM' """
 	logging.basicConfig(level=logging.INFO, format='%(asctime)s -  %(levelname)s-%(message)s')
-	logging.info('Start of program')
+	logging.info('Start of the program')
 
 	try:
 		year = sys.argv[1]
@@ -61,17 +17,36 @@ def main():
 		day = sys.argv[3] 
 	except:
 		logging.info('Wrong date. Check if date is passed as year and month (YYYY, MM, DD)')
-		
+
 	if (int(year) >=2010) & (int(year)<= datetime.datetime.now().year) \
 		& (int(month)<=12) & (int(month)>=1) & (len(month)==2) \
 		& (int(day)<=31) & (int(day)>=1) & (len(day)==2):
 
-		spark = create_spark_session()
-		process_json(year, month, day, spark)
+		path = "{}-{}-{}".format(year, month, str(day).zfill(2))
+
+		if os.path.isdir(path):
+			conf = pyspark.SparkConf().setAppName('Git').setMaster('local')
+			sc = pyspark.SparkContext(conf = conf)
+			spark = SparkSession(sc)
+			logging.info('******* Spark session created ******* ')
+
+			df = spark.read.json(path)
+			df = df.filter(((df.type=="PullRequestEvent") & (df.payload.action=='opened')) | \
+						((df.type=="IssuesEvent") & (df.payload.action=='opened')) | \
+						(df.type=="ForkEvent")) \
+					.selectExpr(["created_at", "actor['id'] as actor_id", "actor['login'] as actor_login", \
+							"actor['display_login'] as display_login","repo['id'] as repo_id", \
+							"repo['name'] as repo_name", "type"])
+
+			output_file = "df_{}.parquet".format(path)
+			df.write.parquet(output_file)
+			logging.info('******* Github data processed successfully. *******')
+		else:
+			logging.info('Directory not exists')
 	else:
-		logging.info('Wrong date. Check if date is passed as year and month (YYYY, MM, DD)')
-		
-	logging.info('End of program')
+		logging.info('Wrong date')
+
+		logging.info('End of program')
 
 if __name__ == "__main__":
-	main()
+		main()
